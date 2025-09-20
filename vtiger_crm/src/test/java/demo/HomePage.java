ca4647fdad136552779fdd6a4510ded7e9b6b141
Manikandan <Manikandan@LAPTOP-9KMGCDCT>
2025-09-20 12:51:02 +0530

    i added the login method

--- vtiger_crm/src/test/java/demo/HomePage.java
+++ vtiger_crm/src/test/java/demo/HomePage.java
@@ -7,10 +7,20 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
+	WebDriver driver;
+	
+	
 	@FindBy(id = "User_name")
 	private WebElement namelink;
 	
+	@FindBy(name = "password")
+	private WebElement passwordlink;
+	
+	@FindBy(partialLinkText = "log in")
+	private WebElement loginBtn;
+	
 	public HomePage(WebDriver driver) {
+		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
 
@@ -18,5 +28,18 @@ public class HomePage {
 		return namelink;
 	}
 	
+	public WebElement getPasswordLink() {
+		return passwordlink;
+	}
+	
+	public WebElement getLoginBtn() {
+		return loginBtn;
+	}
+	
+	public void login(String name , String password) {
+		namelink.sendKeys(name);
+		passwordlink.sendKeys(password);
+		loginBtn.click();
+	}
 	
 }
